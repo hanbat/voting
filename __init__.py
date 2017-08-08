7a131e4fca0bd2e239cff6086c0cd7f8668712b7
Han-Bat Kil <hkil@uwaterloo.ca>
2017-08-08 03:05:26 -0400

    frontend partially done

--- __init__.py
+++ __init__.py
@@ -13,7 +13,6 @@ cnx= {'host': '52.15.80.147',
   'password': 'Uwaterloo',
   'db': 'ece458'}
 
-
 #DB config
 app.config['MYSQL_DATABASE_USER'] = 'ECE458'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'Uwaterloo'
@@ -27,13 +26,13 @@ def check_password(hashed_password, user_password):
 
 def validate(username, password):
     completion = False
-    cur.execute("select username, password from users")
-    res = cur.fetchall()
-    for row in res:
-        dbUser = row[0]
-        dbPass = row[1]
-        if dbUser==username:
-            completion = check_password(dbPass, password)
+    query = "select username, password from users where username = '" + username + "'"
+    cur.execute(query)
+    res = cur.fetchone()
+    dbUser = res[0]
+    dbPass = res[1]
+    if dbUser==username:
+        completion = check_password(dbPass, password)
     return completion
 
 @app.route('/', methods=['GET', 'POST'])
@@ -47,13 +46,42 @@ def login():
         if completion ==False:
             error = 'Invalid Credentials. Please try again.'
         else:
-            return redirect(url_for('vote'))
+            return redirect(url_for('vote', user=username))
     return render_template('login.html', error=error)
 
-@app.route('/vote')
+@app.route('/vote', methods=['POST', 'GET'])
 def vote():
-    
-    return render_template('ballot.html')
+    username = request.args['user']
+    query = "select token from users where username = '" + username + "'"
+    cur.execute(query)
+    res = cur.fetchone()
+    token = res[0]
+
+    if request.method == 'POST':
+        ballot = request.form['candidate']
+        query = "select * from votes where token = '" + token + "'"
+        cur.execute(query)
+        res = cur.fetchone()
+        print res
+        # user haven't voted yet
+        if res is None :
+            query = "insert into votes (token, selection) values ('" + token + "'," + ballot + ")"
+            cur.execute(query)
+            conn.commit()
+        # user already voted..
+        else :
+            print "user ALREADY VOTED!!!"
+        return redirect(url_for('error'))
+    return render_template('ballot.html', user= username)
+
+
+@app.route('/logout')
+def logout():
+    return 'Successfully voted!!';
+
+@app.route('/error')
+def error():
+    return 'You have ALREADY voted!! Your vote did not go through...';    
 
 if __name__ == '__main__':
     app.run(debug=True)
